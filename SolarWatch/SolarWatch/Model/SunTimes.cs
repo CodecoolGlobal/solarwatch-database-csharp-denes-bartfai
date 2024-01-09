5d7d6c540e24bc911adb0b244f8b7d83a2df31c0
denes-bartfai <denes.bartfai@gmail.com>
2024-01-09 12:09:29 +0100

    Create SunTimes model class

---
+++ SolarWatch/SolarWatch/Model/SunTimes.cs
@@ -0,0 +1,11 @@
+﻿namespace SolarWatch.Model;
+
+public class SunTimes
+{
+    public int Id { get; init; }
+    public string? CityName { get; init; }
+    public DateTime Sunrise { get; init; }
+    public DateTime Sunset { get; init; }
+    public DateTime Date { get; init; }
+    
+}
