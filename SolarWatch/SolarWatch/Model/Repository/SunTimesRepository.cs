5b86252908e5df31e33906d27246132e4207767d
denes-bartfai <denes.bartfai@gmail.com>
2024-01-10 18:02:47 +0100

    Create SunTimesRepository class with basic CRUD operations

---
+++ SolarWatch/SolarWatch/Model/Repository/SunTimesRepository.cs
@@ -0,0 +1,45 @@
+﻿using SolarWatch.Data;
+
+namespace SolarWatch.Model.Repository;
+
+public class SunTimesRepository : ISunTimesRepository
+{
+    public IEnumerable<SunTimes> GetAll()
+    {
+        using var dbContext = new SolarWatchContext();
+        return dbContext.SunTimes.ToList();
+    }
+
+    public SunTimes? GetCityName(string cityName)
+    {
+        using var dbContext = new SolarWatchContext();
+        return dbContext.SunTimes.SingleOrDefault(c => c.CityName == cityName);
+    }
+
+    public SunTimes? GetByDateAndName(string cityName, DateTime date)
+    {
+        using var dbContext = new SolarWatchContext();
+        return dbContext.SunTimes.SingleOrDefault(c => c.Date == date && c.CityName == cityName);
+    }
+
+    public void Add(SunTimes city)
+    {
+        using var dbContext = new SolarWatchContext();
+        dbContext.Add(city);
+        dbContext.SaveChanges();
+    }
+
+    public void Delete(SunTimes city)
+    {
+        using var dbContext = new SolarWatchContext();
+        dbContext.Remove(city);
+        dbContext.SaveChanges();
+    }
+
+    public void Update(SunTimes city)
+    {
+        using var dbContext = new SolarWatchContext();
+        dbContext.Update(city);
+        dbContext.SaveChanges();
+    }
+}
